7a1ee3b7719c84d8612dd76bf2b0c7909434cb23
Yan Hui Bryant Tan <s10242090@connect.np.edu.sg>
2023-02-08 03:19:06 +0800

    made signup and login forms functional

--- js/loginsignup.js
+++ js/loginsignup.js
@@ -25,3 +25,75 @@ link.addEventListener("click", e => {
  forms.classList.toggle("show-signup");
 })
 })
+
+// api-key for login information: 63e28b26478852088da67e80 
+async function checkLogin(email, password) {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://idasg2-7926.restdb.io/rest/players?q={"email":"${email}"}`,
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "63e28b26478852088da67e80",
+      "cache-control": "no-cache"
+    }
+  };
+
+  const response = await $.ajax(settings);
+  if (response.length === 0) {
+    return false;
+  } else if (response[0].password === password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+document.getElementById("login-form").addEventListener("submit", async event => {
+  event.preventDefault();
+  const email = document.getElementById("email-address").value;
+  const password = document.getElementById("password").value;
+  const isValid = await checkLogin(email, password);
+  if (isValid) {
+    localStorage.setItem("user", JSON.stringify({ email }));
+    window.location.href = "yes.html";
+  } else {
+    alert("Incorrect email or password");
+  }
+});
+
+function savePlayer(email, password) {
+  var jsondata = {
+    "email": email,
+    "password": password,
+    "xp": 0
+  };
+
+  var settings = {
+    "crossDomain": true,
+    "url": "https://idasg2-7926.restdb.io/rest/players",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "63e28b26478852088da67e80 "
+    },
+    "data": JSON.stringify(jsondata),
+    "beforeSend": function(){
+      $("#sign-up").prop("disabled", true);
+      $("#sign-up-form").trigger("reset");
+    }
+  };
+    
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
+
+document.getElementById('sign-up-form').addEventListener('submit', event => {
+  event.preventDefault();
+  const email = document.getElementById('email-address').value;
+  const password = document.getElementById('password').value;
+  savePlayer(email, password);
+});
+
