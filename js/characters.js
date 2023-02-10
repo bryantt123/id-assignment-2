ec0778a64afda595956ad39691cbf7a76a0c7d7f
Yan Hui Bryant Tan <s10242090@connect.np.edu.sg>
2023-02-10 18:30:02 +0800

    changed character.js to show char image

--- js/characters.js
+++ js/characters.js
@@ -16,25 +16,171 @@ function getCharactersOwned() {
                 const cardsContainer = document.querySelector(".container");
                 let cards = "";
                 chars.forEach(function(item) {
-                cards += `
-                <div class="card">
-                    <div class="card-image">
-                        <img src="${item.charImage}" alt="Character">
-                    </div>
+                    if(item.character === "Yelan"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/yelan.png" alt="Character">
+                            </div>
 
-                    <div class="card-body">
-                        <h2>${item.character}</h2>
-                        <p>${item.charDesc}</p>
-                    </div>
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
 
-                    <div class="selector">
-                        <button>go with this!</button>
-                    </div>
-                </div>`;
-                });
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Yoimiya"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/yoimiya.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Kaeya"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/kaeya.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Kazuha"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/kazuha.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Bennett"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/bennet.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Rosaria"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/rosaria.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Qiqi"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/qiqi.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Xiao"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/xiao.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Mona"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/mona.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    } else if (item.character === "Wanderer"){
+                        cards += `
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="pics/wanderer.png" alt="Character">
+                            </div>
+
+                            <div class="card-body">
+                                <h2>${item.character}</h2>
+                                <p>${item.charDesc}</p>
+                            </div>
+
+                            <div class="selector">
+                                <button>go with this!</button>
+                            </div>
+                        </div>`;
+                    }
                 cardsContainer.innerHTML = cards;
             })
         })
+    })
 }
 
 getCharactersOwned();
