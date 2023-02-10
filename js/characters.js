005ba43c920ba6d58e97e4f619aa05def6fff7ee
Yan Hui Bryant Tan <s10242090@connect.np.edu.sg>
2023-02-10 17:52:13 +0800

    updated characters tab

--- js/characters.js
+++ js/characters.js
@@ -1,42 +1,40 @@
-const characterList = document.querySelector('#character-list');
-
-const fetchCharacters = async () => {
-  try {
+function getCharactersOwned() {
     $.ajax({
         "async": true,
         "crossDomain": true,
         "url": "https://idasg2-fc8e.restdb.io/rest/characters-owned",
         "method": "GET",
         "headers": {
-            "content-type": "application/json",
-            "x-apikey": "63e60003478852088da67ff3",
-            "cache-control": "no-cache"
-        },
-        "processData": false,
-    })
-    .done(function (response) {
-        console.log(response);
-        characters.forEach((character) => {
-            const characterCard = document.createElement('div');
-            characterCard.classList.add('character-card');
+          "content-type": "application/json",
+          "x-apikey": "63e60003478852088da67ff3",
+          "cache-control": "no-cache"
+        }
+      })
+      .done(function(response){
+            const chars = response.filter(char => char.username === JSON.parse(localStorage.getItem('username')));
+            chars.forEach(char => {
+                const cardsContainer = document.querySelector(".container");
+                let cards = "";
+                chars.forEach(function(item) {
+                cards += `
+                <div class="card">
+                    <div class="card-image">
+                        <img src="${item.charImage}" alt="Character">
+                    </div>
 
-            const characterImage = document.createElement('div');
-            characterImage.classList.add('character-image');
-            characterImage.style.backgroundImage = `url(${character.charImage})`;
+                    <div class="card-body">
+                        <h2>${item.character}</h2>
+                        <p>${item.charDesc}</p>
+                    </div>
 
-            const characterName = document.createElement('div');
-            characterName.classList.add('character-name');
-            characterName.innerText = character.character;
-
-            characterCard.appendChild(characterImage);
-            characterCard.appendChild(characterName);
-
-            characterList.appendChild(characterCard);
+                    <div class="selector">
+                        <button>go with this!</button>
+                    </div>
+                </div>`;
+                });
+                cardsContainer.innerHTML = cards;
+            })
         })
-    })
-  } catch (error) {
-    console.error(error);
-  }
-};
+}
 
-fetchCharacters();
+getCharactersOwned();
